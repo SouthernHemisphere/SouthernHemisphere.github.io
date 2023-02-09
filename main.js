d36a853da19a79045e1641ade2a30d5df0df85a0
SouthernHemisphere <ulrich-wittmann@gmx.de>
2023-02-09 21:38:25 +0100

    Farbänderung Bessere Lesbarkeit/ Zentrieren Button

--- main.js
+++ main.js
@@ -1,26 +1,26 @@
 $("document").ready(function() {
-    $("#greeting").html("Alles Ugo bei dir?");
+    $("#greeting").html("Alles Ugo bei dir ?");
     window.nameIsUli = true;
     $("#nameButton").click(function() {
         if (window.nameIsUli) {
             $("#greeting").html("Hallo Felix!");
             window.nameIsUli = false;
         } else {
-            $("#greeting").html("Alles Ugo bei dir?");
+            $("#greeting").html("Alles Ugo bei dir ?");
             window.nameIsUli = true;
         }
     });
 });
 
 $("document").ready(function() {
-    $("#greeting").html("Alles Ugo bei dir?");
+    $("#greeting").html("Alles Ugo bei dir ?");
     window.nameIsUli = true;
     $("#neinButton").click(function() {
         if (window.nameIsUli) {
-            $("#greeting").html("Hallo Das Scheusal!");
+            $("#greeting").html("Hallo, Das Scheusal!");
             window.nameIsUli = false;
         } else {
-            $("#greeting").html("Alles Ugo bei dir?");
+            $("#greeting").html("Alles Ugo bei dir ?");
             window.nameIsUli = true;
         }
     });
