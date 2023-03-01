8d0be54b6c5b91c84856f0b83a44bcce9e1ee29e
SouthernHemisphere <ulrich-wittmann@gmx.de>
2023-03-01 17:24:46 +0100

    Testen mit JS

--- scripts/test.js
+++ scripts/test.js
@@ -1,5 +1,20 @@
-let firstName;
-let lastName;
-firstName= 'Seltas';
-lastName= 'Sommerwind';
+const name = 'Das Scheusal';
+const age = '314';
+const message1 = `Mein Name ist ${name}, ich bin ${age} Jahre alt.`;
+console.log(message1);
+
+const message2 = 'Sehr geehrter Herr Ugo, \n\ngerne senden wir Sie zu ' +
+                'Forschungszwecken in die Tiefen der Arktis.\n' +
+                'Bitte nehmen Sie sich in Acht vor hungrigen Eisbären.\n\n' +
+                'Mit frostigen Grüßen,\nDie Gesellschaft der Pinguine';
+console.log(message2);
+
+const message3 = 
+`
+Post Scriptum:
+
+Vergessen Sie nicht Ihre Zahnbürste!
+Und auf keinen Fall den binomischen Lehrsatz!`
+
+console.log(message3);
 
